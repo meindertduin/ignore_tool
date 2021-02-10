17acf38b2b3446558645416012aaa04f187ff266
meindertduin <meindert@arch-meindert.localdomain>
2021-02-10 13:09:11 +0000

    added ignore files client for retrieving ignore pages

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
-use curl::easy::{Easy, Easy2, Handler, WriteError};
+use curl::{easy::{Easy, Easy2, Handler, WriteError}, multi::Easy2Handle};
 use curl::multi::Multi;
-use std::time::Duration;
+use fmt::write;
+use std::{fmt::{self, Display}, time::Duration};
 
 
 struct EasyCollector {
     buffer: Vec<u8>,
+    ignore_type: IgnoreType,
 }
 
 impl Handler for EasyCollector {
@@ -15,33 +17,76 @@ impl Handler for EasyCollector {
 }
 
 impl EasyCollector {
-    fn new() -> EasyCollector {
+    fn new(ignore_type: IgnoreType) -> EasyCollector {
         EasyCollector {
             buffer: Vec::new(),
+            ignore_type,
         }
     }
 }
 
-pub fn create_new_ingore(path: &String, args: &Vec<String>){ 
-    let mut rust_handle = Easy2::new(EasyCollector::new());
-    rust_handle.url("https://raw.githubusercontent.com/github/gitignore/master/Rust.gitignore").unwrap();
-    rust_handle.get(true).unwrap();
+struct IgnoreFilesClient {
+    multi: Multi,
+    easy_handles: Vec<Easy2Handle<EasyCollector>>,
+}
 
-    let mut c_handle = Easy::new();
-    c_handle.url("https://raw.githubusercontent.com/github/gitignore/master/C%2B%2B.gitignore").unwrap();
+impl IgnoreFilesClient {
+    fn new() -> IgnoreFilesClient {
+        let multi = Multi::new();
 
-    let mut multi = Multi::new();
-    multi.pipelining(true, true).unwrap();
-    let rust_handle = multi.add2(rust_handle).unwrap();
+        let mut multi = Multi::new();
+        multi.pipelining(true, true).unwrap();         
 
-    while multi.perform().unwrap() > 0 {
-        multi.wait(&mut [], Duration::from_secs(1)).unwrap();
+        IgnoreFilesClient {
+            multi,
+            easy_handles: Vec::new(),
+        }
     }
 
-    let handler = rust_handle.get_ref();
-    
-    println!("{}",message);
+    fn add_ignore(&mut self, url: &str, ignore_type: IgnoreType){
+        let mut easy = Easy2::new(EasyCollector::new(ignore_type));
+        easy.url(url).unwrap();
+        easy.get(true).unwrap(); 
+        let easy_handle = self.multi.add2(easy).unwrap();
+        self.easy_handles.push(easy_handle);
+    }
+
+    fn send_requests(&self) -> &Vec<Easy2Handle<EasyCollector>>{
+       while self.multi.perform().unwrap() > 0 {
+            self.multi.wait(&mut [], Duration::from_secs(1)).unwrap();
+       } 
+
+       self.easy_handles.as_ref() 
+    }
 }
+
+#[derive(Debug)]
+enum IgnoreType {
+    Rust,
+    C,
+}
+
+impl Display for IgnoreType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+pub fn create_new_ingore(path: &String, args: &Vec<String>){ 
+    let mut client = IgnoreFilesClient::new();
+    client.add_ignore("https://raw.githubusercontent.com/github/gitignore/master/Rust.gitignore", IgnoreType::Rust);
+    client.add_ignore("https://raw.githubusercontent.com/github/gitignore/master/C.gitignore", IgnoreType::C);
+    let ignore_request_handlers = client.send_requests();
+    
+
+    for handler in ignore_request_handlers {
+        let message = String::from_utf8_lossy(&handler.get_ref().buffer);
+        println!("Handler === {}", handler.get_ref().ignore_type);
+        println!("{}", message);
+    }
+
+}
+
 
 
 
