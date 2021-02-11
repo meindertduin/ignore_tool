8f97a4c58100ef5697a4580dce95db146644e83a
meindertduin <meindert@arch-meindert.localdomain>
2021-02-11 10:47:21 +0000

    implementing dynamic ingore retrieval and seperating in own module

---
+++ src/ignore_client.rs
@@ -0,0 +1,73 @@
+use curl::multi::Multi;
+use curl::{easy::{Easy, Easy2, Handler, WriteError}, multi::Easy2Handle};
+use std::{fmt::{self, Display}, time::Duration};
+
+pub struct IgnoreFilesClient {
+    multi: Multi,
+    easy_handles: Vec<Easy2Handle<EasyCollector>>,
+}
+
+impl IgnoreFilesClient {
+    pub fn new() -> IgnoreFilesClient {
+        let multi = Multi::new();
+
+        let mut multi = Multi::new();
+        multi.pipelining(true, true).unwrap();         
+
+        IgnoreFilesClient {
+            multi,
+            easy_handles: Vec::new(),
+        }
+    }
+
+    pub fn add_ignore(&mut self, ignore_data: IgnoreClientData){
+        let mut easy = Easy2::new(EasyCollector::new(ignore_data.ignore_type));
+        easy.url(ignore_data.url.as_str()).unwrap();
+        easy.get(true).unwrap(); 
+        let easy_handle = self.multi.add2(easy).unwrap();
+        self.easy_handles.push(easy_handle);
+    }
+
+    pub fn send_requests(&self) -> &Vec<Easy2Handle<EasyCollector>>{
+       while self.multi.perform().unwrap() > 0 {
+            self.multi.wait(&mut [], Duration::from_secs(1)).unwrap();
+       } 
+
+       self.easy_handles.as_ref() 
+    }
+}
+
+
+pub struct EasyCollector {
+    pub buffer: Vec<u8>,
+    pub ignore_type: String,
+}
+
+impl Handler for EasyCollector {
+    fn write(&mut self, data: &[u8]) -> Result<usize, WriteError> {
+        self.buffer.extend_from_slice(data);
+        Ok(data.len())
+    }
+}
+
+impl EasyCollector {
+    fn new(ignore_type: String) -> EasyCollector {
+        EasyCollector {
+            buffer: Vec::new(),
+            ignore_type,
+        }
+    }
+}
+
+pub struct IgnoreClientData {
+    pub url: String,
+    pub ignore_type: String,
+}
+
+pub fn cap_first_char(s: &str) -> String {
+    let mut c = s.chars();
+    match c.next() {
+        None => String::new(),
+        Some(f) => f.to_uppercase().collect::<String>() + c.as_str(),
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,12 @@
-use curl::{easy::{Easy, Easy2, Handler, WriteError}, multi::Easy2Handle};
-use curl::multi::Multi;
-use fmt::write;
-use std::{fmt::{self, Display}, time::Duration};
-
+mod ignore_client;
 
 pub fn create_new_ingore(path: &String, args: &Vec<String>){ 
-    let mut client = IgnoreFilesClient::new();
+    let mut client = ignore_client::IgnoreFilesClient::new();
 
     let client_data = get_ignore_client_data(args);
 
     for data in client_data {
-        client.add_ignore(data.0.as_str(), data.1);
+        client.add_ignore(data);
     }
 
     let ignore_request_handlers = client.send_requests();
@@ -22,93 +18,29 @@ pub fn create_new_ingore(path: &String, args: &Vec<String>){
     }
 }
 
-fn get_ignore_client_data(types: &Vec<String>) -> Vec<(String, IgnoreType)> {
-    let mut client_data: Vec<(String, IgnoreType)> = Vec::new();
+fn get_ignore_client_data(types: &Vec<String>) -> Vec<ignore_client::IgnoreClientData> {
+    let mut client_data: Vec<ignore_client::IgnoreClientData> = Vec::new();
 
     let base_ignore_url = "https://raw.githubusercontent.com/github/gitignore/master/";
 
-    for ignore in types {
-        client_data.push((format!("{}{}{}", base_ignore_url, cap_first_char(ignore.as_str()), ".gitignore"), IgnoreType::Rust));
+    for ignore in types 
+    {
+        let cap_ingore_type = ignore_client::cap_first_char(ignore.as_str());
+        
+        client_data.push(ignore_client::IgnoreClientData {
+            url: format!("{}{}{}", base_ignore_url, cap_ingore_type.as_str(), ".gitignore"),
+            ignore_type: cap_ingore_type,
+        });
     } 
 
     client_data
 }
 
-fn cap_first_char(s: &str) -> String {
-    let mut c = s.chars();
-    match c.next() {
-        None => String::new(),
-        Some(f) => f.to_uppercase().collect::<String>() + c.as_str(),
-    }
-}
 
-struct EasyCollector {
-    buffer: Vec<u8>,
-    ignore_type: IgnoreType,
-}
 
-impl Handler for EasyCollector {
-    fn write(&mut self, data: &[u8]) -> Result<usize, WriteError> {
-        self.buffer.extend_from_slice(data);
-        Ok(data.len())
-    }
-}
 
-impl EasyCollector {
-    fn new(ignore_type: IgnoreType) -> EasyCollector {
-        EasyCollector {
-            buffer: Vec::new(),
-            ignore_type,
-        }
-    }
-}
 
-struct IgnoreFilesClient {
-    multi: Multi,
-    easy_handles: Vec<Easy2Handle<EasyCollector>>,
-}
 
-impl IgnoreFilesClient {
-    fn new() -> IgnoreFilesClient {
-        let multi = Multi::new();
-
-        let mut multi = Multi::new();
-        multi.pipelining(true, true).unwrap();         
-
-        IgnoreFilesClient {
-            multi,
-            easy_handles: Vec::new(),
-        }
-    }
-
-    fn add_ignore(&mut self, url: &str, ignore_type: IgnoreType){
-        let mut easy = Easy2::new(EasyCollector::new(ignore_type));
-        easy.url(url).unwrap();
-        easy.get(true).unwrap(); 
-        let easy_handle = self.multi.add2(easy).unwrap();
-        self.easy_handles.push(easy_handle);
-    }
-
-    fn send_requests(&self) -> &Vec<Easy2Handle<EasyCollector>>{
-       while self.multi.perform().unwrap() > 0 {
-            self.multi.wait(&mut [], Duration::from_secs(1)).unwrap();
-       } 
-
-       self.easy_handles.as_ref() 
-    }
-}
-
-#[derive(Debug)]
-enum IgnoreType {
-    Rust,
-    C,
-}
-
-impl Display for IgnoreType {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?}", self)
-    }
-}
 
 
 
